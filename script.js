fd9cef8f84e1a3e2d5ece0bbd813ec0cec5034c6
Jennifer <32855060+jennnico@users.noreply.github.com>
2018-07-02 13:18:38 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+   //On load, hide everything but the Home page.
+  $('#about').hide();
+  $('#products').hide();
+  $('#jokes').hide();
+  
+  //Home page
+  $("#navHome").on("click", function(){
+    $('#home').hide().fadeIn(1000);
+    $('#about').hide();
+    $('#products').hide();
+    $('#jokes').hide();
+  });
+  
+    //About page
+   $("#navAbout").on("click", function(){
+    $('#home').hide();
+    $('#about').hide().fadeIn(1000);
+    $('#products').hide();
+    $('#jokes').hide();
+  });
+  
+   //Products page
+   $("#navProducts").on("click", function(){
+    $('#home').hide();
+    $('#about').hide();
+    $('#products').hide().fadeIn(1000);
+    $('#jokes').hide();
+  });
+  
+  //Jokes page
+   $("#navJokes").on("click", function(){
+    $('#home').hide();
+    $('#about').hide();
+    $('#products').hide();
+    $('#jokes').hide().fadeIn(1000);
+  });
+  
+  
+  });
